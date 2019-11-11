422c4bd531b7a607d2e78ee976933cb797a6dfd5
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-11 23:04:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ def self.genre_count
   #@@genres.group_by(genre)
 
     @@genres.each do |genre|
-      genre_count = genre.count
+      genre_count << genre.count
    #if genre_count[genre] == true
 
 end
